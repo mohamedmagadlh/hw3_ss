bbe31ebaa6ef792831000cac4f6d655edffc62e1
mohamedmagadlh <93886878+mohamedmagadlh@users.noreply.github.com>
2021-12-20 22:32:35 +0200

    update

--- functions.h
+++ functions.h
@@ -3,10 +3,10 @@
 #define WORD 30
 # define TXT 1024
 void funcA(char txt[TXT],char Word[WORD],char res[TXT]);
-void funcB(char txt[TXT],char atbshWord[WORD],char res[TXT]);
+void funcB(char txt[TXT],char Word[WORD]);
 void funcC(char txt[TXT],char word[WORD]);
 int GematriaWord( char  Word[WORD]);
 int GematriaChar( char  ch);
 void func (char txt[TXT],char res[TXT],int t,int i,int j);
 void *AtbshWord(char str[TXT]);
-void reverce(char word[WORD],char rev[WORD]);
+char *reverse(char Word[WORD]);
